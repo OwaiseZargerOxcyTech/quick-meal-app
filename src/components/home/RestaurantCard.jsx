c544ecb623ef89fe3b0dcd9e1855098a4cdfaf58
owaisezarger <owaisezarger@gmail.com>
2024-03-28 13:09:33 +0530

    swiggy api

---
+++ src/components/home/RestaurantCard.jsx
@@ -0,0 +1,28 @@
+import { CDN_URL } from "@/utils/constants";
+
+const RestaurantCard = (props) => {
+  const { resData } = props;
+
+  const { cloudinaryImageId, name, avgRating, cuisines, areaName } =
+    resData?.info;
+
+  return (
+    <div className="card w-60 shadow-xl hover:shadow-2xl transition duration-300">
+      <figure>
+        <img
+          className="res-logo "
+          alt="res-logo"
+          src={CDN_URL + cloudinaryImageId}
+        />
+      </figure>
+      <div className="card-body">
+        <h4 className="card-title text-base">{name}</h4>
+        <h6 className="text-sm font-semibold">{avgRating} stars</h6>
+        <p className="text-sm truncate">{cuisines.join(", ")}</p>
+        <p className="text-sm truncate">{areaName}</p>
+      </div>
+    </div>
+  );
+};
+
+export default RestaurantCard;
